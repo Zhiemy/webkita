2b01ab87b7eb97d8f80cb4182cf2b0c543114210
Aji Mulyana <kuliwebs@gmail.com>
2025-02-11 08:19:34 +0700

    Penambahan fitur bukutamu

---
+++ bukutamu.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bukutamu</title>
+</head>
+<body>
+    <h1>Ini adalah fitur untuk bukutamu</h1>
+</body>
+</html>
